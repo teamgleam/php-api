7a7294de218708923250ce378cfd4c40aef27406
Matt Sutton <mattsutton@Matts-MacBook-Pro.local>
2023-11-02 08:47:30 +0000

    update me endpoint

--- src/User.php
+++ src/User.php
@@ -6,16 +6,11 @@ class User extends GleamBase
 {
     public function me()
     {
-        return $this->_call( 'get', 'users/me' );
+        return $this->_call( 'get', 'me' );
     }
 
     public function device_create( $params = [] )
     {
-        return $this->_call( 'post', 'users/me/devices', $params );
-    }
-
-    public function retrieve( $id )
-    {
-        return $this->_call( 'get', 'users/' . $id );
+        return $this->_call( 'post', 'me/devices', $params );
     }
 }
