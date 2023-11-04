aecb87af39adf262facddb0e478040faa56a453d
Matt Sutton <mattsutton@Matts-MacBook-Pro.local>
2023-11-04 15:29:54 +0000

    add refund

--- src/Transaction.php
+++ src/Transaction.php
@@ -4,8 +4,13 @@ namespace Gleam;
 
 class Transaction extends GleamBase
 {
-    public function retrieve( $invoice_id )
+    public function retrieve( $transaction_id )
     {
-        return $this->_call( 'get', 'transactions/' . $invoice_id );
+        return $this->_call( 'get', 'transactions/' . $transaction_id );
+    }
+
+    public function refund( $transaction_id, $params = [] )
+    {
+        return $this->_call( 'post', 'transactions/' . $transaction_id . '/refund', $params );
     }
 }
